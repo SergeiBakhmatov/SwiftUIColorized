b31dcb06de11063737cb7a64afc5bafd39fa9966
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-06-12 22:42:07 +0500

    work. No alert

--- SwiftUIColorized/ContentView.swift
+++ SwiftUIColorized/ContentView.swift
@@ -13,10 +13,17 @@ struct ContentView: View {
     @State private var greenValue = Double.random(in: 1...255)
     @State private var blueValue = Double.random(in: 1...255)
     
+    @State private var valueRedTF = 0.0
+    @State private var valueGreenTF = 0.0
+    @State private var valueBlueTF = 0.0
+    
     @State private var colorRed: Color = .red
     @State private var colorGreen: Color = .green
     @State private var colorBlue: Color = .blue
     
+    @FocusState private var redIsFocused: Bool
+    @FocusState private var greenIsFocused: Bool
+    @FocusState private var blueIsFocused: Bool
     
     var body: some View {
         ZStack {
@@ -31,15 +38,86 @@ struct ContentView: View {
                     colorBlueValue: blueValue
                 )
                 
-                ColorPickerView(value: $redValue, color: $colorRed)
-                ColorPickerView(value: $greenValue, color: $colorGreen)
-                ColorPickerView(value: $blueValue, color: $colorBlue)
+                HStack {
+                    ColorPickerView(value: $redValue, color: $colorRed)
+                    TextField("0",
+                              value: redIsFocused ? $valueRedTF : $redValue,
+                              format: .number.rounded(
+                                rule: .toNearestOrAwayFromZero,
+                                increment: 1.0)
+                    )
+                    .frame(width: 50, height: 30)
+                    .background()
+                    .multilineTextAlignment(.trailing)
+                    .cornerRadius(3)
+                    .foregroundColor(.black)
+                    .keyboardType(.numberPad)
+                    .focused($redIsFocused)
+                }
+                HStack {
+                    ColorPickerView(value: $greenValue, color: $colorGreen)
+                    TextField("0",
+                              value: greenIsFocused ? $valueGreenTF : $greenValue,
+                              format: .number.rounded(
+                                rule: .toNearestOrAwayFromZero,
+                                increment: 1.0)
+                    )
+                    .frame(width: 50, height: 30)
+                    .background()
+                    .multilineTextAlignment(.trailing)
+                    .cornerRadius(3)
+                    .foregroundColor(.black)
+                    .keyboardType(.numberPad)
+                    .focused($greenIsFocused)
+                }
+                HStack {
+                    ColorPickerView(value: $blueValue, color: $colorBlue)
+                    TextField("0",
+                              value: blueIsFocused ? $valueBlueTF : $blueValue,
+                              format: .number.rounded(
+                                rule: .toNearestOrAwayFromZero,
+                                increment: 1.0)
+                    )
+                    .frame(width: 50, height: 30)
+                    .background()
+                    .multilineTextAlignment(.trailing)
+                    .cornerRadius(3)
+                    .foregroundColor(.black)
+                    .keyboardType(.numberPad)
+                    .focused($blueIsFocused)
+                }
                 
                 Spacer()
+                
+            }
+            .toolbar {
+                ToolbarItem(placement: .keyboard) {
+                    Button("Done", action: action)
+                        .padding(EdgeInsets(top: 0, leading: 300, bottom: 0, trailing: 0))
+                }
             }
             .padding()
         }
+        .onTapGesture {
+            redIsFocused = false
+            greenIsFocused = false
+            blueIsFocused = false
+        }
         
+    }
+    private func action() {
+        if redIsFocused {
+            redValue = valueRedTF
+            redIsFocused.toggle()
+        }
+        if greenIsFocused {
+            greenValue = valueGreenTF
+            greenIsFocused.toggle()
+        }
+        if blueIsFocused {
+            blueValue = valueBlueTF
+            blueIsFocused.toggle()
+        }
     }
 }
 
@@ -58,7 +136,6 @@ struct ColorPickerView: View {
         HStack {
             ValueTextView(value: value)
             SliderView(value: $value, tintColor: $color)
-            ValueTFView(value: $value)
         }
     }
 }
@@ -83,32 +160,3 @@ struct ColorView: View {
     }
 }
 
-struct ValueTFView: View {
-    
-    @Binding var value: Double
-    
-    var body: some View {
-        
-        TextField("0",
-                  value: $value,
-                  format: .number.rounded(
-                    rule: .toNearestOrAwayFromZero,
-                    increment: 1.0)
-        )
-            .frame(width: 50, height: 30)
-            .background()
-            .multilineTextAlignment(.trailing)
-            .cornerRadius(3)
-            .foregroundColor(.black)
-            .keyboardType(.numberPad)
-            .toolbar {
-                ToolbarItem(placement: .keyboard) {
-                    Button("Done", action: action)
-                }
-            }
-            .foc
-
-    }
-    
-    private func action() {}
-}
