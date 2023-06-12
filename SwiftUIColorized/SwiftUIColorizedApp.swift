ccb0d71639a973e4f7f17e08f974f21e0a42dc6c
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-06-12 12:52:00 +0500

    Initial Commit

---
+++ SwiftUIColorized/SwiftUIColorizedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIColorizedApp.swift
+//  SwiftUIColorized
+//
+//  Created by Sergei Bakhmatov on 12.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIColorizedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
